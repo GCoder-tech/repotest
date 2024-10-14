2c231dfed2cc9aa1ff7d093374b815b72a5f93ba
GCoder-tech <reddyarun8088@gmail.com>
2024-10-14 11:05:56 -0500

    Update script

--- .github/scripts/cascading-dropdowns.js
+++ .github/scripts/cascading-dropdowns.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 
 const data = {
-    "key1": "value1",
-    "key2": "value2",
-    "key3": "value3"
+    "DEE": "1001.510050.9976",
+    "DRCB": "1001.510050.9326",
+    "DLFI": "3002.510050.9722",
+    "DBTVS": "1001.510050.9775",
+    "BPS": "1001.510050.9770",
+    "DBUS": "3801.510050.9975" 
 };
 
-const selectedKey = process.env.DROPDOWN_1;
+const selectedKey = process.env.value-stream-dd;
 let selectedValue = '';
 
 if (selectedKey && data[selectedKey]) {
     selectedValue = data[selectedKey];
 }
+
+process.env.buc-dd = selectedValue;
